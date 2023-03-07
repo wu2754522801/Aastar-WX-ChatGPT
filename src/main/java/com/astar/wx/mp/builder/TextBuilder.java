be2a3ad22bde5814d7483020fcb1a784e0e5e379
wuzhenyong <young_zya@163.com>
2023-03-07 09:47:55 +0800

    FEAT-优化调用次数

--- src/main/java/com/astar/wx/mp/builder/TextBuilder.java
+++ src/main/java/com/astar/wx/mp/builder/TextBuilder.java
@@ -13,14 +13,16 @@ import java.util.Map;
  * Astar（一颗星）
  */
 public class TextBuilder extends AbstractBuilder {
-    Map<String, Integer> dataMap = new HashMap<>();
+    public  static  final Map<String, Integer> dataMap = new HashMap<>();
 
 
     @Override
     public WxMpXmlOutMessage build(String content, WxMpXmlMessage wxMessage,
                                    WxMpService service) {
         if ("我的信息".equals(wxMessage.getContent())) {
-            WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT().content(wxMessage.getFromUser())
+            String text = "用户：" + wxMessage.getFromUser() + "\n" +
+                "剩余次数：" + dataMap.get(wxMessage.getFromUser());
+            WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT().content(text)
                 .fromUser(wxMessage.getToUser()).toUser(wxMessage.getFromUser())
                 .build();
             return m;
@@ -34,9 +36,9 @@ public class TextBuilder extends AbstractBuilder {
                 WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT().content("次数不足，请联系Q：2754522801获取免费次数")
                     .fromUser(wxMessage.getToUser()).toUser(wxMessage.getFromUser())
                     .build();
+                dataMap.put(wxMessage.getFromUser(), count);
                 return m;
             }
-            dataMap.put(wxMessage.getFromUser(), count);
         } else {
             if (wxMessage.getContent().contains("充值用户")) {
                 // 充值用户:ou9Qi531biZIdwQb28fRbNRNQtts-100
@@ -45,6 +47,10 @@ public class TextBuilder extends AbstractBuilder {
                 String openId = user[0];
                 Integer cishu = Integer.parseInt(user[1]);
                 dataMap.put(openId, dataMap.get(wxMessage.getFromUser()).intValue() + cishu.intValue());
+                WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT().content("充值成功：余额：" + dataMap.get(wxMessage.getFromUser()))
+                    .fromUser(wxMessage.getToUser()).toUser(wxMessage.getFromUser())
+                    .build();
+                return m;
             }
         }
         WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT().content(OpenAIAPI.chat(wxMessage.getContent()))
